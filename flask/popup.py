95ddaee7075b5eb55187d9da5800340833aebdad
Ullas B K <coding.ullas@gmail.com>
2024-04-25 02:33:24 +0530

    Update popup.py

--- flask/popup.py
+++ flask/popup.py
@@ -4,6 +4,26 @@ from bs4 import BeautifulSoup
 
 from googlesearch import search
 
+import re
+
+def extract_domain(url):
+  # Define a regular expression pattern for extracting the domain
+  pattern = r"(https?://)?(www\d?\.)?(?P<domain>[\w\.-]+\.\w+)(/\S*)?"
+  # Use re.match to search for the pattern at the beginning of the URL
+  match = re.match(pattern, url)
+  # Check if a match is found
+  if match:
+  # Extract the domain from the named group "domain"
+    domain = match.group("domain")
+    return domain
+  else:
+    return None
+
+# Example usage
+# domain1 = extract_domain("https://www.example.com/path/to/page")
+# domain2 = extract_domain("http://sub.example.co.uk/some-page")
+# print(f"Domain 1: {domain1}")
+# print(f"Domain 2: {domain2}")
 def check_for_popups(url):
     try:
         # Fetch the webpage content
@@ -34,7 +54,7 @@ def check_for_popups(url):
 #         print("The URL does not have pop-ups.")
 def get_page_rank(url):
 
-    query = input('get me' + url)
+    query =  extract_domain(url).split('.')
 # initilazing index=0
     index = 0
     flag = False
@@ -43,20 +63,20 @@ def get_page_rank(url):
 # here, tid is top level domain, 
 # stop means how many search results you want
 # stop=100 means we will get top 100 results from google
-    for i in search(query,        # The query you want to run  
-                    tld = None,  # The top level domain  
-                    lang = 'en',  # The language  
-                    num = 10,     # Number of results per page  
-                    start = 0,    # First result to retrieve  
-                    stop = None,  # Last result to retrieve  
-                    pause = 2.0,  # Lapse between HTTP requests  
+    for i in search(query[0],        # The query you want to run  
+                      # The top level domain  
+                    lang = 'en'  # The language  
+                   # num = 10,     # Number of results per page  
+                    #start = 0,    # First result to retrieve  
+                    #stop = 100,  # Last result to retrieve  
+                    #pause = 2.0,  # Lapse between HTTP requests  
                 ):  
     
         if url in i:
             # printing index of website and url
             flag = True
              
-        index += 1
+        
     if(flag):
         return 1
     else:
